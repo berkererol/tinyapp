42afed8e51c097141937bd9ef36bc629c7e19c9c
berkererol <berkererol@gmail.com>
2019-09-03 01:37:30 +0000

    Enabled EJS on views and partials.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const ejs = require('ejs')
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set('view engine', 'ejs');
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,4 +25,15 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const { shortURL } = req.params;
+  let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
+  res.render("urls_show", templateVars);
 });
