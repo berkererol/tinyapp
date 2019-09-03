b0a9946983acd9921eb2908bed9d8a261ce17b97
berkererol <berkererol@gmail.com>
2019-09-03 02:06:37 +0000

    Added a form to submit URLs to be shorthened.

--- express_server.js
+++ express_server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const ejs = require('ejs')
 const app = express();
 const PORT = 8080; // default port 8080
-
-app.set('view engine', 'ejs');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true })); // Enables body-parse
+app.set('view engine', 'ejs'); // Enables EJS for rendering the pages
 
 
 const urlDatabase = {
@@ -32,8 +33,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const { shortURL } = req.params;
-  let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
+  let templateVars = { shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = function () {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (var i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+
+}
